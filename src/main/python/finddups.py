80ba63347ac4846a2744fb30d6297af6f8a50e40
HarveyBates <phenobottle@gmail.com>
2021-10-05 15:40:41 +1100

    sort by uid

--- src/main/python/finddups.py
+++ src/main/python/finddups.py
@@ -2,6 +2,7 @@ import json
 import mysql.connector
 import os
 import os.path
+import sys
 import psycopg2
 
 from collections import deque
@@ -15,26 +16,26 @@ with open("config.json", "r") as configFile:
 
 postgresCfg = config["postgres"]
 
-# Define prevous month here
 currentDate = date(year=2021, month=10, day=1)
-previousDate = currentDate - timedelta(days=32) # Current date minus just over a month
+previousDate = currentDate - timedelta(days=32)
 
 batchSize = 10000
 
 lastMsg = {}
-
 
 def getPostgresConnection():
     print("Connecting to database...")
     return psycopg2.connect(host=postgresCfg["host"], port=postgresCfg["port"], user=postgresCfg["user"], password=postgresCfg["password"], dbname=postgresCfg["database"])
 
 
-def getNextBatch(cursor, deveui, offset):
-    cursor.execute("select uid, msg from msgs where deveui = %s and ((coalesce(msg->'metadata'->>'time', msg->'uplink_message'->>'recieved_at'))::timestamptz >= %s) order by uid limit %s offset %s", (deveui, previousDate, batchSize, offset))
+def getNextBatch(cursor, deveui, uid):
+    print("Getting next batch")
+    cursor.execute("select uid, msg from msgs where deveui = %s and uid >= %s order by uid limit %s", (deveui, uid, batchSize))
     result = cursor.fetchall()
     return result
 
-def findDupes(conn, deveui):
+
+def findDupes(conn, deveui, minUid):
     dupCount = 0
     offset = 0
 
@@ -46,35 +47,40 @@ def findDupes(conn, deveui):
         with conn.cursor() as cursor:
             print("#", end="", flush=True)
             i = 0
-            for (uid, msg) in getNextBatch(cursor, deveui, offset):
-                i += 1
-                if msg not in recentMsgs:
-                    recentMsgs.append(msg)
-                    continue
+            try:
+                for (uid, msg) in getNextBatch(cursor, deveui, minUid):
+                    i += 1
+                    minUid = uid
+                    if msg not in recentMsgs:
+                        recentMsgs.append(msg)
+                        continue
 
-                dupCount = dupCount + 1
+                    dupCount = dupCount + 1
 
-                with conn.cursor() as updateCursor:
-                    updateCursor.execute("update msgs set (ignore, reason) = (%s, 'Duplicate in RawData') where uid = %s", (True, uid))
+                    with conn.cursor() as updateCursor:
+                        updateCursor.execute("update msgs set (ignore, reason) = (%s, 'Duplicate in RawData') where uid = %s", (True, uid))
 
-            conn.commit()
+                conn.commit()
 
-            offset += i
-            if i < batchSize:
-                break
+                offset += i
+                if i < batchSize:
+                    break
+            except Exception as e:
+                print(e)
+                sys.exit()
 
     print("")
     print(f"Found {dupCount} duplicates.")
+    return dupCount
 
 
 with getPostgresConnection() as conn:
     with conn.cursor() as devQry:
-        devQry.execute("select distinct(deveui) from msgs")
+        devQry.execute("select deveui, min(uid) from msgs WHERE (ts >= %s) GROUP BY deveui", (previousDate,))
         nDups = 0
-        for (deveui, ) in devQry.fetchall():
+        for (deveui, minUid) in devQry.fetchall():
             print(deveui)
-            devDups = findDupes(conn, deveui)
-            nDups += devDups
-        print(f"A total of {nDups} were found.")
-
+            dups = findDupes(conn, deveui, minUid)
+            nDups += dups
+        print(f"Found a total of {nDups} duplicates.")
 
