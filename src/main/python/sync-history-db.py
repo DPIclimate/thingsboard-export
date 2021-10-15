51da04b8b550b1ed21b0a06f54209709cac14246
David Taylor <david.a.taylor@dpi.nsw.gov.au>
2021-10-15 15:28:38 +1100

    History db sync uses executemany.

--- src/main/python/sync-history-db.py
+++ src/main/python/sync-history-db.py
@@ -1,9 +1,7 @@
 import json
 import logging
-import time
 
 import mysql.connector
-import os
 
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
 
@@ -17,7 +15,17 @@ history_config = config["mysql"]
 fdt_broker_config = config["fdtBroker"]
 
 
-def get_last_rawdata_uid(db_cfg: dict) -> int:
+def get_mysql_connection(db_cfg: dict):
+    return mysql.connector.connect(host=db_cfg['host'], port=db_cfg['port'], user=db_cfg['user'],
+                                   password=db_cfg['password'], database=db_cfg['database'])
+
+
+fdt_connection = get_mysql_connection(fdt_broker_config)
+history_connection = get_mysql_connection(history_config)
+
+
+# Temporarily passing in table name so we can use a different table name in the history db while developing.
+def get_last_rawdata_uid(connection, table_name) -> int:
     """
     Find the highest UID from the RawData table in the database described by db_cfg.
 
@@ -27,107 +35,49 @@ def get_last_rawdata_uid(db_cfg: dict) -> int:
     Returns:
         the highest UID from the RawData table in the database described by db_cfg.
     """
-
-    with mysql.connector.connect(host=db_cfg['host'], port=db_cfg['port'], user=db_cfg['user'],
-                                 password=db_cfg['password'], database=db_cfg['database']) as all_msgs_conn:
-        with all_msgs_conn.cursor(buffered=True) as all_msgs_cursor:
-            all_msgs_cursor.execute("select max(uid) from RawData")
-            rs = all_msgs_cursor.fetchone()
-            return rs[0]
+    with connection.cursor(buffered=True) as all_msgs_cursor:
+        all_msgs_cursor.execute(f"select max(uid) from {table_name}")
+        rs = all_msgs_cursor.fetchone()
+        return rs[0]
 
 
-def process_batch(db_cfg_broker: dict, db_cfg_endpoint: dict,start_uid: int) -> dict:
-    """
+def process_batch(start_uid: int) -> dict:
+    with fdt_connection.cursor(buffered=True) as fdt_cursor:
+        fdt_cursor.execute("select uid, payload, state from RawData where uid > %s order by uid limit 2000", (start_uid, ))
+        row_list = fdt_cursor.fetchall()
+        row_count = len(row_list)
+        last_row = row_list[-1]
 
-    Args:
-        db_cfg:
-        start_uid:
+        with history_connection.cursor() as history_cursor:
+            history_cursor.executemany("insert into RawDataTest(uid, payload, state) values (%s, %s, %s)", row_list)
+            history_connection.commit()
 
-    Returns:
-        maxUid: The last Uid that was inserted to the historical database.
-        rowCount: The amount of rows inserted into the historical database.
-    """
+    return {"maxUid": last_row[0], "rowCount": row_count}
 
-    row_count = 0
-    max_uid = start_uid
-    with mysql.connector.connect(host=db_cfg_broker['host'], port=db_cfg_broker['port'], user=db_cfg_broker['user'],
-                                 password=db_cfg_broker['password'], database=db_cfg_broker['database']) as all_msgs_conn:
-        with all_msgs_conn.cursor(buffered=True) as all_msgs_cursor:
-            all_msgs_cursor.execute("select uid, payload, state from RawData where uid > %s order by uid limit 10", (start_uid, ))
-            for (uid, payload, state) in all_msgs_cursor.fetchall():
-                max_uid = uid
-                row_count += 1
-                print(uid, state, payload[:180])
-                record = (uid, payload)
-                insert_rawdata_row(history_config, record)
-    return {"maxUid": max_uid, "rowCount": row_count}
-
-def insert_rawdata_row(db_cfg_historical: dict, row: tuple) -> int:
-    """
-    What does it do?
-
-    Args:
-        db_cfg: a database configuration section from the config.json file.
-
-    Returns:
-
-    """
-    try:
-        hist_connection = mysql.connector.connect(host=db_cfg_historical['host'], port=db_cfg_historical['port'], user=db_cfg_historical['user'], password=db_cfg_historical['password'], database=db_cfg_historical['database'])
-
-        histInsertSQL = """INSERT INTO public.testmsgs (uid, msg) VALUES (%s, %s) """
-        print(histInsertSQL)
-        hist_cursor = hist_connection.cursor()
-        uid, payload = row
-        print(f"Inserting row: {uid}")
-        result = hist_cursor.execute(histInsertSQL, row)
-        hist_connection.commit()
-
-        print(f"Row {uid} inserted successfully")
-
-    except mysql.connector.Error as error:
-        hist_connection.rollback()
-        print("Failed to insert into MySQL table {}".format(error))
-
-    return
 
 def main():
     #os.chdir("/tmp")
 
-    broker_uid = get_last_rawdata_uid(fdt_broker_config)
-    backup_uid = get_last_rawdata_uid(history_config)
+    broker_uid = get_last_rawdata_uid(fdt_connection, "RawData")
+    history_uid = get_last_rawdata_uid(history_connection, "RawDataTest")
 
     log.info(f"Broker max uid: {broker_uid}")
-    log.info(f"Backup max uid: {backup_uid}")
-    log.info(f"Msgs to Sync: {broker_uid - backup_uid}")
+    log.info(f"History max uid: {history_uid}")
+    log.info(f"Msgs to Sync: {broker_uid - history_uid}")
 
-    if broker_uid <= backup_uid:
+    if broker_uid <= history_uid:
         log.info("History version of RawData is up-to-date, skipping sync.")
         return
     else:
-        while backup_uid < broker_uid:
-            log.info("Copying tables..") # Start is 5505237 for backup db
-            curr_batch = process_batch(fdt_broker_config, history_config, backup_uid)
+        while history_uid < broker_uid:
+            log.info("Starting batch")
+            curr_batch = process_batch(history_uid)
             log.info(f"Processed {curr_batch['rowCount']} rows, up to Uid: {curr_batch['maxUid']}")
-            backup_uid = get_last_rawdata_uid(history_config)
-            log.info(f"Msgs left to Sync: {broker_uid - backup_uid}")
-            time.sleep(10) # Sleep for 10 seconds between each batch
-
+            history_uid = get_last_rawdata_uid(history_connection, "RawDataTest")
+            log.info(f"Msgs left to Sync: {broker_uid - history_uid}")
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-"""
-    row_count = 0
-    start_uid = backup_uid
-    while row_count < 99:
-        rc = process_batch(fdt_broker_config, start_uid)
-        start_uid = rc["maxUid"]
-        row_count += rc["rowCount"]
-"""
+    fdt_connection.close()
+    history_connection.close()
